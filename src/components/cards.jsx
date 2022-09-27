8e63b1e99e2c6100bcaaf3d78135f75c8128086e
Iacopo Poli <iapoli@lighton.ai>
2022-09-27 16:24:57 +0200

    :sparkles: Major redesign of the prompt design guide

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -11,3 +11,17 @@ export const Card = ({link, title, description}) => {
         </a>
     )
 };
+
+export const CardNoLink = ({title, prompt, output}) => {
+    return (
+        <div style={{background: "#272729", borderRadius: "6.9%", padding: "7.89%", height: "100%", verticalAlign: "middle"}}>
+            <div>
+                <h2>{title}</h2>
+                <div style={{whiteSpace: 'pre-line'}}>🧑 <b>{prompt}</b></div>
+            </div>
+            <div style={{background: '#524D63', borderRadius: "10px", padding: "3.945%"}}>
+                <div style={{whiteSpace: 'pre-line'}}>🤖 {output}</div>
+            </div>
+        </div>
+    )
+};
